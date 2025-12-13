9b439aa96ecb2cc7c82f8cf92b8574cc5278f081
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2025-12-13 14:45:10 +0000

    bug

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', [FrontController::class, 'index'])->middleware(['auth'])->name('home');
+Route::get('/dashboard', [FrontController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 Route::get('/', function () {
     return view("front.index");
