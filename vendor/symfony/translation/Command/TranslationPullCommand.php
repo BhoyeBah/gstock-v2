22909543e246512127a279c888db477caed49711
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2026-05-08 17:38:36 +0000

    suppression de debugbar

--- vendor/symfony/translation/Command/TranslationPullCommand.php
+++ vendor/symfony/translation/Command/TranslationPullCommand.php
@@ -98,21 +98,21 @@ final class TranslationPullCommand extends Command
                 new InputOption('as-tree', null, InputOption::VALUE_REQUIRED, 'Write messages as a tree-like structure. Needs --format=yaml. The given value defines the level where to switch to inline YAML'),
             ])
             ->setHelp(<<<'EOF'
-The <info>%command.name%</> command pulls translations from the given provider. Only
-new translations are pulled, existing ones are not overwritten.
+                The <info>%command.name%</> command pulls translations from the given provider. Only
+                new translations are pulled, existing ones are not overwritten.
 
-You can overwrite existing translations (and remove the missing ones on local side) by using the <comment>--force</> flag:
+                You can overwrite existing translations (and remove the missing ones on local side) by using the <comment>--force</> flag:
 
-  <info>php %command.full_name% --force provider</>
+                  <info>php %command.full_name% --force provider</>
 
-Full example:
+                Full example:
 
-  <info>php %command.full_name% provider --force --domains=messages --domains=validators --locales=en</>
+                  <info>php %command.full_name% provider --force --domains=messages --domains=validators --locales=en</>
 
-This command pulls all translations associated with the <comment>messages</> and <comment>validators</> domains for the <comment>en</> locale.
-Local translations for the specified domains and locale are deleted if they're not present on the provider and overwritten if it's the case.
-Local translations for others domains and locales are ignored.
-EOF
+                This command pulls all translations associated with the <comment>messages</> and <comment>validators</> domains for the <comment>en</> locale.
+                Local translations for the specified domains and locale are deleted if they're not present on the provider and overwritten if it's the case.
+                Local translations for others domains and locales are ignored.
+                EOF
             )
         ;
     }
@@ -163,7 +163,7 @@ EOF
                 $this->writer->write($operation->getResult(), $format, $writeOptions);
             }
 
-            $io->success(sprintf('Local translations has been updated from "%s" (for "%s" locale(s), and "%s" domain(s)).', parse_url($provider, \PHP_URL_SCHEME), implode(', ', $locales), implode(', ', $domains)));
+            $io->success(\sprintf('Local translations has been updated from "%s" (for "%s" locale(s), and "%s" domain(s)).', parse_url($provider, \PHP_URL_SCHEME), implode(', ', $locales), implode(', ', $domains)));
 
             return 0;
         }
@@ -177,7 +177,7 @@ EOF
             $this->writer->write($catalogue, $format, $writeOptions);
         }
 
-        $io->success(sprintf('New translations from "%s" has been written locally (for "%s" locale(s), and "%s" domain(s)).', parse_url($provider, \PHP_URL_SCHEME), implode(', ', $locales), implode(', ', $domains)));
+        $io->success(\sprintf('New translations from "%s" has been written locally (for "%s" locale(s), and "%s" domain(s)).', parse_url($provider, \PHP_URL_SCHEME), implode(', ', $locales), implode(', ', $domains)));
 
         return 0;
     }
